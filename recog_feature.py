df4785df4cacb49513660663bea4ff2a4d15cc00
nrm <gustavonrm@gmail.com>
2021-11-17 02:19:05 +0000

    start

--- recog_feature.py
+++ recog_feature.py
@@ -10,12 +10,12 @@ keyRealCoords = {}
 # Initiate SIFT detector
 
 img1 = cv.imread('images/keyboard.png', 0)  # queryImage
-
-sift = cv.SIFT_create()
-fast = cv.FastFeatureDetector_create()
+#sift = cv.SIFT_create()
+#fast = cv.FastFeatureDetector_create()
+orb = cv.ORB_create()
 #kp1, des1 = sift.detectAndCompute(img1, None)
-kp1 = fast.detect(img1, None)
-des1 = sift.compute(img1, kp1)[1]
+kp1 = orb.detect(img1, None)
+kp1, des1 = orb.compute(img1, kp1)
 
 
 # surf = cv.surf(400)
@@ -44,50 +44,71 @@ def feature_detection(frame):
 
     # find the keypoints and descriptors with SIFT
     #kp2, des2 = sift.detectAndCompute(img2, None)
-    kp2 = fast.detect(img2, None)
-    des2 = sift.compute(img2, kp2)[1]
+    kp2 = orb.detect(img2, None)
+    kp2, des2 = orb.compute(img2, kp2)
     # kp2, des2 = surf.detectAndCompute(img2, None)
 
-    FLANN_INDEX_KDTREE = 1
-    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-    search_params = dict(checks=50)
-    flann = cv.FlannBasedMatcher(index_params, search_params)
+    #FLANN_INDEX_KDTREE = 1
+    #index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    #search_params = dict(checks=50)
+    #flann = cv.FlannBasedMatcher(index_params, search_params)
 
-    matches = flann.knnMatch(des1, des2, k=2)
+    #matches = flann.knnMatch(des1, des2, k=2)
+    # create BFMatcher object
+    bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+# Match descriptors.
+    matches = bf.match(des1, des2)
+# Sort them in the order of their distance.
+    matches = sorted(matches, key=lambda x: x.distance)
+
+    # create BFMatcher object
+    #bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+# Match descriptors.
+    #matches = bf.knnMatch(des1, des2, k=20)
+# Sort them in the order of their distance.
+    #matches = sorted(matches, key=lambda x: x.distance)
+    # store all the good matches as per Lowe's ratio test.
 
     # store all the good matches as per Lowe's ratio test.
+
+    for match in matches:
+        p1 = kp1[match.queryIdx].pt
+        p2 = kp2[match.trainIdx].pt
+
     good = []
-    for m, n in matches:
-        if m.distance < 0.7*n.distance:
-            good.append(m)
 
-    if len(good) > MIN_MATCH_COUNT:
-        src_pts = np.float32(
-            [kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-        dst_pts = np.float32(
-            [kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
-
-        M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
-
-        if np.array(M).size == 1:
-            return
-
-        for key, value in keyCoords.items():
-            pts = np.float32(np.array(value)).reshape(-1, 1, 2)
-            dst = cv.perspectiveTransform(pts, M)
-            img2 = cv.polylines(img2, [np.int32(dst)],
-                                True, 255, 3, cv.LINE_AA)
-            keyRealCoords[key] = np.int32(dst)
+    # for m in matches:
+    #    if m.distance < 75:
+    #        good.append(m)
+#
+    # if len(good) > MIN_MATCH_COUNT:
+    #    src_pts = np.float32(
+    #        [kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+    #    dst_pts = np.float32(
+    #        [kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+#
+    #    M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
+#
+    #    if np.array(M).size == 1:
+    #        return
+#
+    #    for key, value in keyCoords.items():
+    #        pts = np.float32(np.array(value)).reshape(-1, 1, 2)
+    #        dst = cv.perspectiveTransform(pts, M)
+    #        img2 = cv.polylines(img2, [np.int32(dst)],
+    #                            True, 255, 3, cv.LINE_AA)
+    #        keyRealCoords[key] = np.int32(dst)
 
     # print(keyRealCoords)
     img2 = cv.circle(img2, (200, 200), 2, (0, 0, 255), 2)
+    img2 = cv.drawKeypoints(img2, kp2, None, color=(0, 255, 0), flags=0)
     cv.imshow("features", img2)
 
 
 # MAIN
 # Replace the below URL with your own. Droidcam keep '/video'
 url = "http://192.168.1.24:4747/video"
-vid = cv.VideoCapture(0)
+vid = cv.VideoCapture(url)
 # detect_key(keyCoords, 1, 1)
 
 while(True):
@@ -95,6 +116,7 @@ while(True):
     # Capture the video frame
     # by frame
     ret, frame = vid.read()
+    #b = cv.resize(frame, (1280, 720), fx=0, fy=0, interpolation=cv.INTER_CUBIC)
     feature_detection(frame)
     # cv.imshow("features", frame)
     # Detect keyboard
